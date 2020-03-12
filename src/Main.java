ac7f027b4cb1b04e9bbc829d6a362a5760bc9e7d
ALEXANDER <1119manuelpoma@gmail.com>
2020-03-12 11:48:14 -0400

    metodo para hallar determinante 2x2 y 3x3

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@ import Matriz.Matriz;
 
 public class Main {
      public static void main(String[] args) {
-         double[][] x= {{1,10,3,1},{1,5,1.5,1},{1,10,1,1},{1,20,2,1},{1,15,4,1},{1,10,2,1}};
-         Matriz matriz=new Matriz(x.length,x[0].length,x);
-         matriz.showMatriz();
+         double[][] x= {{1,11,3,1},{1,5,1.5,2},{1,10,1,1},{1,20,2,1}};
+         double[][] x2= {{3,10,5},{1,5,4},{2,3,8}};
+
+         Matriz matriz=new Matriz();
+        // matriz.showMatriz();
+
+         System.out.println("MAtrix inicial es: ");
+         matriz.mostrarMatriz(x2);
+         System.out.println("El determinante de la matriz es: "+matriz.determinanteMatriz(x));
+         //matriz.mostrarMatriz();
+
+        // System.out.println("El determinante es: "+matriz.determinanteMatriz(x2));
 
     }
 }
--- src/Matriz/Matriz.java
+++ src/Matriz/Matriz.java
@@ -19,6 +19,9 @@ public class Matriz {
 		this.matrizInt = matrizInt;
 	}
 
+	public Matriz() {
+	}
+
 	public int getNumberRows() {
 		return numberRows;
 	}
@@ -52,7 +55,7 @@ public class Matriz {
 	}
 
 
-	public void showMatriz(double[][] matriz) {
+	public void mostrarMatriz(double[][] matriz) {
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[0].length; j++) {
 				System.out.print(matriz[i][j] + "   ");
@@ -61,7 +64,7 @@ public class Matriz {
 		}
 	}
 
-	public double[][] transposedMatriz(double[][] matriz) {
+	public double[][] transpuestaMatriz(double[][] matriz) {
 
 		double[][] transposedResult = new double[getMatrizDouble()[0].length][getMatrizDouble().length];
 		for (int i = 0; i < matriz.length; i++) {
@@ -71,6 +74,70 @@ public class Matriz {
 		}
 		return transposedResult;
 	}
+
+	public double[][] multiplicacionMatriz(double[][] matriz1, double[][] matriz2) {
+		double[][] multiplicationResult = new double[matriz1.length][matriz2[0].length];
+		if (matriz1[0].length == matriz2.length) {
+
+		} else {
+			throw new RuntimeException("No se puede multiplicar las matrices");
+		}
+
+		return multiplicationResult;
+	}
+
+
+	public double determinanteMatriz(double[][] matriz){
+		double determinante=0;
+		double suma1=0,suma2=0;
+		double[][] matriz2=new double[matriz.length][matriz.length+matriz.length-1];
+
+		if (matriz.length==matriz[0].length){
+			for (int i=0;i<matriz2.length;i++){
+				for (int j=0;j<matriz2[0].length;j++){
+					if (j>=matriz[0].length){
+						matriz2[i][j]=matriz[i][j-matriz.length];
+					}if (j<matriz[0].length){
+						matriz2[i][j]=matriz[i][j];
+					}
+				}
+			}
+			System.out.println("La matriz es cuadrada");
+				for (int j=0;j<matriz2.length;j++){
+					double multiplicacion=1;
+					int contador=0,ii=0,jj=j;
+					while (contador<matriz2.length){
+							multiplicacion=multiplicacion*matriz2[ii][jj];
+						System.out.println("Se multiplicara la posicion ["+ii+" , "+jj+"]");
+							ii++;
+							jj++;
+							contador++;
+					}
+					suma1=suma1+multiplicacion;
+				}
+
+			System.out.println("RESTAS: \n");
+				for(int j=0;j<matriz2.length;j++){
+					double multiplicacion=1;
+					int contador=0,ii=matriz.length-1,jj=j;
+					while (contador<matriz2.length){
+						multiplicacion=multiplicacion*matriz2[ii][jj];
+						System.out.println("Se multiplicara la posicion ["+ii+" , "+jj+"] con valor: "+matriz2[ii][jj]);
+						ii--;
+						jj++;
+						contador++;
+					}
+					suma2=suma2+multiplicacion;
+					System.out.println(suma2);
+				}
+
+		}else {
+			throw new RuntimeException("La matriz no es cuadrada");
+		}
+		return suma1-suma2;
+	}
+
+
 
 
 }
